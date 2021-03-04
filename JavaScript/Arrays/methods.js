a746278c40746ac41e5b7b17c181da5e651b797f
Seth Lewandowski <sethlew@protonmail.com>
2021-03-04 15:22:25 -0700

    array methods 1 video notes

---
+++ JavaScript/Arrays/methods.js
@@ -0,0 +1,12 @@
+// push, pop, unshift, shift
+// let's manipulate arrays in place
+
+const ARRAY = ['second', 'third']
+
+ARRAY.unshift('firs')
+console.log(ARRAY)
+
+console.log(ARRAY.shift())
+ARRAY.unshift('first')
+
+console.log(ARRAY)
